5c8640fa96ed07834f249b69c1ddce29eb589232
Carlos Goicoechea <Carlos.Goicoechea>
2020-12-12 16:54:14 +0100

    Update database script

--- src/main/java/com/course/springboot/repositories/commons/ConstantsUrl.java
+++ src/main/java/com/course/springboot/repositories/commons/ConstantsUrl.java
@@ -5,7 +5,6 @@ public class ConstantsUrl {
     /**
      * Endpoints
      */
-    public static final String API_USERS = "/users";
     public static final String API_EMPLOYEES = "/employees";
 
     /**
